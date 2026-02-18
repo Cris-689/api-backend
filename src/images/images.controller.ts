7ed5179c34b09bd765d054adec6f93c0d06f82da
Cris-689 <cristian.artero.mol@gmail.com>
2026-02-18 17:09:28 +0100

    Mae mia

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,4 +1,8 @@
-import { Controller, Get, Param, Res, HttpStatus, NotFoundException } from '@nestjs/common';
+import { 
+  Controller, Get, Post, Param, Res, 
+  UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator 
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import type { Response } from 'express';
 import { ImagesService } from './images.service';
 
@@ -6,10 +10,28 @@ import { ImagesService } from './images.service';
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file')) // 'file' es el nombre del campo en el formulario
+  async uploadImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 5 }), // Máximo 5MB
+          new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }), // Solo imágenes
+        ],
+      }),
+    ) file: Express.Multer.File,
+  ) {
+    const savedImage = await this.imagesService.create(file);
+    return {
+      id: savedImage.id,
+      message: 'Imagen subida con éxito a perritos_db',
+    };
+  }
+
   @Get(':id')
   async getImage(@Param('id') id: string, @Res() res: Response) {
     const image = await this.imagesService.findOne(+id);
-    
     if (!image) return res.status(404).send('No existe');
 
     res.setHeader('Content-Type', image.mimetype);
--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Image } from './entities/image.entity'; // IMPORTA TU ENTIDAD
+import { Image } from './entities/image.entity';
 
 @Injectable()
 export class ImagesService {
@@ -10,7 +10,16 @@ export class ImagesService {
     private imagesRepository: Repository<Image>,
   ) {}
 
-  // AÑADE EL TIPO DE RETORNO : Promise<Image> 
+  // Método para guardar la imagen en la base de datos
+  async create(file: Express.Multer.File): Promise<Image> {
+    const newImage = this.imagesRepository.create({
+      filename: file.originalname,
+      mimetype: file.mimetype,
+      data: file.buffer, // Aquí guardamos los bytes de la imagen
+    });
+    return await this.imagesRepository.save(newImage);
+  }
+
   async findOne(id: number): Promise<Image | null> {
     return await this.imagesRepository.findOneBy({ id });
   }
